76bf56ae2a1db40784b83bbe5d419de3ccf9831e
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2023-10-01 01:03:35 +0800

    implementing pydantic

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/test")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/demoposts")
+def demo_post(thisIsJustAReferenceName:post):
+    return {"message": "this is a post",
+            "newpost": f"title: {thisIsJustAReferenceName.title} content :{thisIsJustAReferenceName.content}"
+            }
+
+# implementing pydantic for data validation and parsing
