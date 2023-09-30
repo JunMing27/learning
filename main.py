bb925337b669d66f3fde33e7eac10188e1dc78ab
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2023-10-01 01:34:34 +0800

    simulate database using array

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
 class post(BaseModel):
     title: str
     content: str
+    published: bool = True # this will assign default value
+
+my_posts = [{"title":"title of post", "content": "ipsum ", "id": 1},
+            {"title":"title of post", "content": "ipssum ", "id": 2}
+            ]
 
 @app.get("/test")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": my_posts}
 
 @app.post("/demoposts")
 def demo_post(thisIsJustAReferenceName:post):
-    return {"message": "this is a post",
-            "newpost": f"title: {thisIsJustAReferenceName.title} content :{thisIsJustAReferenceName.content}"
-            }
+    # return {"message": "this is a post",
+    #         "newpost": f"title: {thisIsJustAReferenceName.title} content :{thisIsJustAReferenceName.content}"
+    #         }
+    newDict = thisIsJustAReferenceName.dict()
+    newDict['id']= randrange(0,1000000)
+    my_posts.append(newDict)
+    return{"data":newDict}
 
 # this is without pydantic 
 # @app.post("/demoposts")
