aa5f9a7e5e81b21285ef5e3cb99316f05996aa35
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2023-10-01 01:04:31 +0800

    add example

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ class post(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
-
 @app.post("/demoposts")
 def demo_post(thisIsJustAReferenceName:post):
     return {"message": "this is a post",
             "newpost": f"title: {thisIsJustAReferenceName.title} content :{thisIsJustAReferenceName.content}"
             }
+
+# this is without pydantic 
+# @app.post("/demoposts")
+# def demo_post(payload: dict = Body(...)):
+#     print(payload)
+#     return {"message": "this is a post",
+#             "newpost": f"title: {payload['title']} content :{payload['content']}"
+#             }
 
 # implementing pydantic for data validation and parsing
