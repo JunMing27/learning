86fc296e36827b103629c371241558b52e7cf827
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2023-10-01 13:30:25 +0800

    manipulate HTTP status

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -18,7 +18,7 @@ my_posts = [{"title":"title of post", "content": "ipsum ", "id": 1},
 async def root():
     return {"message": my_posts}
 
-@app.post("/demoposts")
+@app.post("/demoposts", status_code=status.HTTP_201_CREATED)
 def demo_post(thisIsJustAReferenceName:post):
     # return {"message": "this is a post",
     #         "newpost": f"title: {thisIsJustAReferenceName.title} content :{thisIsJustAReferenceName.content}"
@@ -26,7 +26,8 @@ def demo_post(thisIsJustAReferenceName:post):
     newDict = thisIsJustAReferenceName.dict()
     newDict['id']= randrange(0,1000000)
     my_posts.append(newDict)
-    return{"data":newDict}
+    return{"data":newDict,
+           "all": my_posts}
 
 # this is without pydantic 
 # @app.post("/demoposts")
@@ -37,3 +38,18 @@ def demo_post(thisIsJustAReferenceName:post):
 #             }
 
 # implementing pydantic for data validation and parsing
+
+@app.get("/demoposts/{id}")
+def getpost(id: int,response:Response): #fastapi can validate and convert variable to int 
+
+    post=find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f" id {id} is not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"post detail": f"id {id} is not found"}
+    return {"post detail": post}
+
+def find_post(id):
+    for i in my_posts:
+        if i['id'] == id:
+            return i
